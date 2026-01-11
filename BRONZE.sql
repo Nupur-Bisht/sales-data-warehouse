ea4431c7428abb1d1cc7ba3c9351ac62a58274a3
Nupur-Bisht <nupurbisht1608@gmail.com>
2026-01-11 13:19:09 +0530

    Innitial commit

---
+++ BRONZE.sql
@@ -0,0 +1,134 @@
+use DataWarehouse;
+go
+--CREATING BRONZE LAYER TABLE 
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
+
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
+GO
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+GO
+
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
+GO
+
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
+GO
+
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt DATE,
+    sls_ship_dt  DATE,
+    sls_due_dt   DATE,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+GO
+
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
+GO
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid    NVARCHAR(50),
+    cntry  NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
+GO
+
+CREATE TABLE bronze.erp_cust_az12 (
+    cid    NVARCHAR(50),
+    bdate  DATE,
+    gen    NVARCHAR(50)
+);
+GO
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+GO
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+    id           NVARCHAR(50),
+    cat          NVARCHAR(50),
+    subcat       NVARCHAR(50),
+    maintenance  NVARCHAR(50)
+);
+ GO
+ --INSERTING VALUES IN TABLE 
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+PRINT '================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '================================================';
+ Truncate table bronze.crm_cust_info;
+ BULK Insert bronze.crm_cust_info
+ From 'C:\Users\nupur\Downloads\Warehouse_project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+ with ( FIRSTROW=2,
+       FIELDTERMINATOR=',',
+	   TABLOCK
+);
+ 
+
+ Truncate table bronze.crm_prd_info;
+ BULK Insert bronze.crm_prd_info
+ From 'C:\Users\nupur\Downloads\Warehouse_project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+ with ( FIRSTROW=2,
+       FIELDTERMINATOR=',',
+	   TABLOCK
+);
+
+ Truncate table bronze.crm_sales_details;
+ BULK Insert bronze.crm_sales_details
+ From 'C:\Users\nupur\Downloads\Warehouse_project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+ with ( FIRSTROW=2,
+       FIELDTERMINATOR=',',
+	   TABLOCK
+);
+
+ TRUNCATE TABLE bronze.erp_cust_az12;
+ BULK INSERT bronze.erp_cust_az12
+ from 'C:\Users\nupur\Downloads\Warehouse_project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+ with( FIRSTROW=2,
+      FIELDTERMINATOR=',',
+	  TABLOCK);
+
+  TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+  BULK INSERT bronze.erp_px_cat_g1v2
+ from 'C:\Users\nupur\Downloads\Warehouse_project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+  with( FIRSTROW=2,
+      FIELDTERMINATOR=',',
+	  TABLOCK);
+
+  TRUNCATE TABLE bronze.erp_loc_a101;
+  BULK INSERT bronze.erp_loc_a101
+  from 'C:\Users\nupur\Downloads\Warehouse_project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+  with( FIRSTROW=2,
+      FIELDTERMINATOR=',',
+  	  TABLOCK);
+  
+END
---
+++ SCHEMA.sql
@@ -0,0 +1,12 @@
+use master;
+go
+create database DataWarehouse;
+USE DataWarehouse;
+--SCHEMA
+CREATE SCHEMA bronze;
+go
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
+
